47e4168e0bbf0363e43108c4726e425410afbb12
Susie Son <susie@stytch.com>
2022-12-07 13:11:51 -0800

    add biometrics to profile page

--- pages/ProfilePage.tsx
+++ pages/ProfilePage.tsx
@@ -1,10 +1,37 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import sharedStyles from "./shared";
-import { useStytch } from "@stytch/react-native";
-import * as React from 'react'
+import {Text, TouchableOpacity, View} from 'react-native';
+import sharedStyles from './shared';
+import {useStytch} from '@stytch/react-native-testing';
+import * as React from 'react';
 
 function ProfilePage() {
   const stytch = useStytch();
+
+  const registerBiometrics = () => {
+    stytch.biometrics
+      .register('Register Your Biometric Factor')
+      .then((resp: any) => {
+        console.log(resp);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+
+  const authBiometrics = () => {
+    stytch.biometrics
+      .authenticate('Register Your Biometric Factor', 60)
+      .then((resp: any) => {
+        console.log(resp);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+
+  const deleteBiometricsDeviceRegistration = async () => {
+    const response = await stytch.biometrics.removeRegistration();
+    console.log(response);
+  };
 
   return (
     <View style={sharedStyles.container}>
@@ -16,7 +43,34 @@ function ProfilePage() {
           app on Github to learn how to implement this yourself!
         </Text>
       </View>
-      <TouchableOpacity style={[sharedStyles.buttonDark]} onPress={stytch.session.revoke}>
+      <View style={{padding: 10, marginBottom: 10}}>
+        <TouchableOpacity
+          style={sharedStyles.buttonDark}
+          onPress={registerBiometrics}>
+          <Text style={sharedStyles.buttonTextDark}>
+            Register with Biometrics
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{padding: 10, marginBottom: 10}}>
+        <TouchableOpacity
+          style={sharedStyles.buttonDark}
+          onPress={authBiometrics}>
+          <Text style={sharedStyles.buttonTextDark}>Auth with Biometrics</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{padding: 10, marginBottom: 10}}>
+        <TouchableOpacity
+          style={sharedStyles.buttonDark}
+          onPress={deleteBiometricsDeviceRegistration}>
+          <Text style={sharedStyles.buttonTextDark}>
+            Delete Biometrics Registration
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={[sharedStyles.buttonDark]}
+        onPress={stytch.session.revoke}>
         <Text style={[sharedStyles.buttonTextDark]}>Logout</Text>
       </TouchableOpacity>
     </View>
